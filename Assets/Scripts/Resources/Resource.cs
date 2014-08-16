018e8ae35aca2eece25e46bc0c809002b6a56989
Adam Liborio <AdamLiborio@gmail.com>
2014-08-16 09:17:53 -0400

    Adding initial resource classes

--- Assets/Scripts/Resources/Resource.cs
+++ Assets/Scripts/Resources/Resource.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class Resource : MonoBehaviour {
+	// All raw resources which can be harvested from blocks will be of this type
+	// Once extracted they will become items (whether they have been refined into
+	// products or not.
 
 	// Use this for initialization
 	void Start () {
