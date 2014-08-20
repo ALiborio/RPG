236d37077d428903318175461721a4a4aee8843d
Adam Liborio <AdamLiborio@gmail.com>
2014-08-19 22:04:02 -0400

    MapBuilder was unnecessary. Map will build itself

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -6,9 +6,41 @@ public class Map : MonoBehaviour{
 	// Stores all the current blocks on this map instance
 	Block[,] blocks;
 	public int height,width;
+	// Block Types:
+	public Block beach;
+	public Block desert;
+	public Block field;
+	public Block forest;
+	public Block mountain;
+	public Block ocean;
+	public Block pond;
+	public Block woods;
+
 	// Use this for initialization
 	void Start() {
 		blocks = new Block[height,width];
+		Block thisBlock;
+		for(int i=0; i<width; i++)
+		{ for(int j=0; j<height; j++) 
+			{
+				if (i==0 || j==0 || i==(width-1) || j==(height-1))
+				{
+					thisBlock = Instantiate(ocean) as Block;
+				}
+				else
+				{
+					if (i==1 || j == 1 || i ==(width-2) || j==(height-2))
+					{
+						thisBlock = oceanOrBeach();
+					}
+					else
+					{
+						thisBlock = getNextBlock();
+					}
+				}
+				addBlock(i,j,thisBlock);
+			}
+		}
 	}
 
 	// Update is called once per frame
@@ -25,6 +57,53 @@ public class Map : MonoBehaviour{
 		blocks[a,b] = block;
 		block.transform.localPosition = new Vector3(a,b,0);
 
+	}
+
+	Block oceanOrBeach()
+		// Returns an ocean or beach block
+	{
+		int type = Mathf.RoundToInt(Random.value);
+		
+		if (type == 1)
+		{
+			return Instantiate(beach) as Block;
+		}
+		else
+		{
+			return Instantiate(ocean) as Block;
+		}
+	}
+	
+	Block getNextBlock()
+		// Generates the next block according to the rules
+		// For now, just randomly picks a block type
+	{
+		Block block;
+		int type = Mathf.FloorToInt(6*Random.value);
+		switch(type)
+		{
+		case 0:
+			block = pond;
+			break;
+		case 1:
+			block = desert;
+			break;
+		case 2:
+			block = field;
+			break;
+		case 3:
+			block = forest;
+			break;
+		case 4:
+			block = mountain;
+			break;
+		default:
+			block = woods;
+			break;
+		}
+		block = Instantiate(block) as Block;
+		return block;
+		
 	}
 
 }
